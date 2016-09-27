85676c7e79f1b1cc1bc0c52699674f189de8b0ca
wanghao <wanghao@wanghaodemacbook-pro.local>
2016-09-27 17:13:12 +1000

    Create new classes for map itinerary feature.

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by wanghao on 27/09/2016.
+//  Copyright © 2016 wanghao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by wanghao on 27/09/2016.
+//  Copyright © 2016 wanghao. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
