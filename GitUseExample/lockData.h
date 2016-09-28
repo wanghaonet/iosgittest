02e0cba26df59bcb7f3708008dfe78022496d2b4
wanghao <wanghao@wanghaodemacbook-pro.local>
2016-09-28 17:23:17 +1000

    Add lockData

---
+++ GitUseExample/lockData.h
@@ -0,0 +1,13 @@
+//
+//  lockData.h
+//  GitUseExample
+//
+//  Created by wanghao on 28/09/2016.
+//  Copyright © 2016 wanghao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lockData : NSObject
+
+@end
---
+++ GitUseExample/lockData.m
@@ -0,0 +1,13 @@
+//
+//  lockData.m
+//  GitUseExample
+//
+//  Created by wanghao on 28/09/2016.
+//  Copyright © 2016 wanghao. All rights reserved.
+//
+
+#import "lockData.h"
+
+@implementation lockData
+
+@end
